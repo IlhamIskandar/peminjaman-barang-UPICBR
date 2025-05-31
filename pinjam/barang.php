fd2f76d9684df1af6c50ef6234f808ae9fec75b5
IlhamIskandar <muhammadilhamiskandar1@gmail.com>
2025-05-31 09:51:28 +0700

    tambah halaman peminjaman

--- pinjam/barang.php
+++ pinjam/barang.php
@@ -2,6 +2,43 @@
 include "koneksi.php";
 include "login-validation.php";
 
+if (isset($_GET['id'])) {
+  $stmt = $conn->prepare("SELECT * FROM barang b JOIN kategori k ON b.id_kategori = k.id_kategori WHERE id_barang=? ");
+  $stmt->bind_param("i", $_GET['id']);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  if (mysqli_num_rows($result) > 0) {
+    $data_barang = mysqli_fetch_assoc($result);
+  } else {
+    echo "
+    <script>
+      alert('Data Barang tidak ditemukan');
+      window.location.href = 'index.php';
+    </script>
+    ";
+  }
+  $stmt = $conn->prepare("SELECT * FROM users WHERE id_user=? ");
+  $stmt->bind_param("i", $_SESSION['id']);
+  $stmt->execute();
+  $user_result = $stmt->get_result();
+  if (mysqli_num_rows($user_result) > 0) {
+    $data_user = mysqli_fetch_assoc($user_result);
+  } else {
+    echo "
+    <script>
+      alert('Data Pengguna tidak ditemukan');
+      window.location.href = 'index.php';
+    </script>
+    ";
+  }
+} else {
+  echo "
+  <script>
+    alert('Pengguna tidak ditemukan');
+    window.location.href = 'index.php';
+  </script>
+  ";
+}
 ?>
 
 <!doctype html>
@@ -47,7 +84,7 @@ include "login-validation.php";
           <div class="container-fluid">
             <!--begin::Row-->
             <div class="row">
-              <div class="col-sm-6"><h3 class="mb-0">Daftar Barang</h3></div>
+              <div class="col-sm-6"><h3 class="mb-0">Pinjam Barang</h3></div>
               <div class="col-sm-6">
               </div>
             </div>
@@ -58,46 +95,74 @@ include "login-validation.php";
         <!--begin::App Content-->
         <div class="app-content">
           <div class="container-fluid" id="dynamic-content">
-                    <?php
-                    $stmt = $conn->prepare("SELECT * FROM barang b JOIN kategori k ON b.id_kategori = k.id_kategori");
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    $n = 1;
-                    while($data= mysqli_fetch_array($result)) {
-                    ?>
             <div class="card mb-3">
               <div class="row card-body ">
                 <div class="col-md-3 col-lg-2 col-sm-12 d-flex align-items-center">
-                  <img class="rounded mx-auto img-fluid" style="" src="../image/barang/<?= $data['img']?>" alt="<?= $data['img']?>">
+                  <img class="rounded mx-auto img-fluid" style="" src="../image/barang/<?= $data_barang['img']?>" alt="<?= $data_barang['img']?>">
                 </div>
                 <div class="col">
                   <div class="row">
-                    <h4 class="mb-0"><?= $data["nama_barang"]?></h4>
+                    <h4 class="mb-1">Informasi Barang</h4>
                   </div>
                   <div class="row">
-                     <p class="mb-1 text-muted">
-                      <?= $data["nama_kategori"]?>
-                     </p>
+                    <div class="col-2 mb-1">
+                      Nama Barang
+                    </div>
+                    <div class="col mb-1">
+                      : <?= $data_barang["nama_barang"]?>
+                    </div>
                   </div>
                   <div class="row">
-                    <p>
-                      <?= $data["deskripsi"]?>
-                    </p>
+                    <div class="col-2 mb-1">
+                      Kategori
+                    </div>
+                    <div class="col mb-1">
+                      : <?= $data_barang["nama_kategori"]?>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      Deskripsi Barang
+                      </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <?= $data_barang["deskripsi"]?>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <h4>Informasi Peminjam</h4>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <?= $data_user["username"]?>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <?= $data_user["email"]?>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col">
+                      <?= $data_user["notelp"]?>
+                    </div>
                   </div>
                   <div class="row align-items-center">
                     <div class="col-6">
                       <p class="mb-0">
-                        Tersedia: <span class="badge bg-success"><?= $data["stok"]?></span>
+                        Tersedia: <span class="badge bg-success"><?= $data_barang["stok"]?></span>
                       </p>
                     </div>
                     <div class="col-6 justify-content-end d-flex">
-                        <a href="barang.php?id=<?= $data['id_barang']?>" class="btn btn-primary">Pinjam</a>
+                        <a href="barang.php?id=<?= $data_barang['id_barang']?>" class="btn btn-primary">Pinjam</a>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-                <?php $n++;} ?>
           </div>
         </div>
         <!--end::App Content-->
